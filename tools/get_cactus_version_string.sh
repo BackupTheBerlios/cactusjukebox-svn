34bef392b6ab79a0478872479e208ecd273afdc5
da_seeb <da_seeb@9dfe511a-3d2b-0410-8695-cae4d89a1d7a>
2009-07-26 18:43:44 +0000

    sk: updated Makefiles

--- tools/get_cactus_version_string.sh
+++ tools/get_cactus_version_string.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-cat source/cactus_const.inc | grep ' *CACTUS_VERSION *=.*;' | sed -e 's/const....[A-Z_ =;]*.//g' -e 's/.;//g' -e's/ //g'
+# argument must be path to cactus_const.inc
+cat $1 | grep ' *CACTUS_VERSION *=.*;' | sed -e 's/const....[A-Z_ =;]*.//g' -e 's/.;//g' -e's/ //g'
